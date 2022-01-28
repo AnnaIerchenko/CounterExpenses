31a5598e0062135fe998bcb01211de0322d0be63
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2022-01-28 07:35:23 -0800

    Initial Commit

---
+++ CounterExpenses/AddView.swift
@@ -0,0 +1,47 @@
+//
+//  AddView.swift
+//  CounterExpenses
+//
+//  Created by Ierchenko Anna  on 1/24/22.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    
+    static let types = ["Bussines", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                    Text($0)
+                }
+            }
+        
+        TextField("Price", text: $amount)
+            .keyboardType(.numberPad)
+    }
+            .navigationBarTitle("Add")
+            .navigationBarItems(trailing: Button("save") {
+                if let actualAmount = Int(self.amount) {
+                    let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                    self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+            })
+        }
+}
+}
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
---
+++ CounterExpenses/CounterExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterExpensesApp.swift
+//  CounterExpenses
+//
+//  Created by Ierchenko Anna  on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct CounterExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
